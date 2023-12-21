e47892a0bceaf652cf3f7f6beff01f2ecc2da6ee
Raul Moreno Gines <raul.moreno.gines@gmail.com>
2023-12-21 13:22:47 +0100

    my first blank template

--- my_personal_web_site_with_reflex/my_personal_web_site_with_reflex.py
+++ my_personal_web_site_with_reflex/my_personal_web_site_with_reflex.py
@@ -1,12 +1,45 @@
-"""Welcome to Reflex!."""
-
-from my_personal_web_site_with_reflex import styles
-
-# Import all the pages.
-from my_personal_web_site_with_reflex.pages import *
+"""Welcome to Reflex! This file outlines the steps to create a basic app."""
+from rxconfig import config
 
 import reflex as rx
 
-# Create the app and compile it.
-app = rx.App(style=styles.base_style)
+docs_url = "https://reflex.dev/docs/getting-started/introduction"
+filename = f"{config.app_name}/{config.app_name}.py"
+
+
+class State(rx.State):
+    """The app state."""
+
+    pass
+
+
+def index() -> rx.Component:
+    return rx.fragment(
+        rx.color_mode_button(rx.color_mode_icon(), float="right"),
+        rx.vstack(
+            rx.heading("Raúl Moreno Ginés", font_size="2em"),
+            rx.box("Perfil personal", rx.code(filename, font_size="1em")),
+            rx.link(
+                "Check out our docs!",
+                href=docs_url,
+                border="0.1em solid",
+                padding="0.5em",
+                border_radius="0.5em",
+                _hover={
+                    "color": rx.color_mode_cond(
+                        light="rgb(107,99,246)",
+                        dark="rgb(179, 175, 255)",
+                    )
+                },
+            ),
+            spacing="1.5em",
+            font_size="2em",
+            padding_top="10%",
+        ),
+    )
+
+
+# Add state and page to the app.
+app = rx.App()
+app.add_page(index)
 app.compile()
